eb1136c34310eeb93f76f0b54b791389e6a4cf5f
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-04-23 11:46:17 +0200

    funcionando correctamente

--- db/seed.ts
+++ db/seed.ts
@@ -3,6 +3,6 @@ import { db, Cliente } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Cliente).values([
-		{ id: 1, name: 'John Doe' },
+		{ id: 2, name: 'Joel' },
 	])
 }
