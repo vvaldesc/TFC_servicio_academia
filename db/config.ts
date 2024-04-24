78aaa5a30332a74b33df4f836e280ed3811bd6e1
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-04-24 08:27:54 +0200

    creando tablas

--- db/config.ts
+++ db/config.ts
@@ -2,7 +2,7 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 const Clients = defineTable({
   columns: {
-    id: column.number({primaryKey: true}),
+    ID: column.number({primaryKey: true}),
     name: column.text({default: 'indefinido'}),
     surname: column.text({default: 'indefinido'}),
     email: column.text({unique: true, optional: true}),
@@ -21,20 +21,42 @@ const Clients = defineTable({
 
 const Stundents = defineTable({
   columns: {
-    'matriculation number': column.number({primaryKey: true}),
+    'matriculation number': column.text({primaryKey: true}),
+    ID: column.number({ references: () => Clients.columns.ID }),
+    DNI: column.text({  }),
+    employed: column.boolean({default: false}),
+  },
+});
+
+const Teacher = defineTable({
+  columns: {
+    NIP: column.number({primaryKey: true}),
+    isAdmin: column.boolean({ default: false }),
+    ID: column.number({ references: () => Clients.columns.ID }),
+  },
+});
+
+const Subject = defineTable({
+  columns: {
+    name: column.text({primaryKey: true}),
+    acronym: column.text({ }),
+    ID: column.number({ references: () => Clients.columns.ID }),
   },
 });
 
 const Employee = defineTable({
   columns: {
     'social security': column.text({primaryKey: true}),
+    Salary: column.number({ default: 0 }),
+    'matriculation number': column.text({ references: () => Stundents.columns['matriculation number'] }),
+    NIP: column.number({ references: () => Teacher.columns.NIP }),
   },
 });
 
 const Services = defineTable({
   columns: {
     ID: column.number({primaryKey: true}),
-    'social security': column.date({ references: () => Employee.columns['social security'] }),
+    'social security': column.text({ references: () => Employee.columns['social security'] }),
     name: column.text(),
     price: column.number(),
     duration: column.number(),
@@ -46,5 +68,5 @@ const Services = defineTable({
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: { Clients, Stundents, Employee, Services}
+  tables: { Clients, Stundents, Employee, Services, Teacher, Subject}
 });
